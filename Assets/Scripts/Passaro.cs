7c7bdab3bc2db983aa43bbfe77ef43d9c3c04129
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-08 09:26:26 -0300

    fisica do passaro

---
+++ Assets/Scripts/Passaro.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passaro : MonoBehaviour
+{
+    Rigidbody2D fisica;
+    private void Awake()
+    {
+      this.fisica = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            this.impulsionar();
+        }
+    }
+    void impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up*3,
+            ForceMode2D.Impulse);
+    }
+}
