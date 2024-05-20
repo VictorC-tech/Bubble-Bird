76ca098a880deffc54a04b4ac5a41adfdc017957
Victor Cecilio <victor020gacha@gmail.com>
2024-05-20 11:21:18 -0300

    Continuando Projeto

---
+++ Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade = 0.6f;
+
+    private Vector3 posI;
+    private float realSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+       this.posI = this.transform.position;
+       float imageSize = GetComponent<SpriteRenderer>().size.x;
+       float escala = this.transform.localScale.x;
+       this.realSize = imageSize * escala;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = Mathf.Repeat(this.velocidade * Time.time, imageSize);
+        this.transform.position = this.posI + Vector3.left * deslocamento;
+    }
+}
